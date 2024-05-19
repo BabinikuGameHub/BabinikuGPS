26c7229d3645293297525e345a7da957cfa285d8
호시아 <hoshia55555@gmail.com>
2024-05-19 16:47:13 +0900

    위 아래 메뉴 스크립트 추가

---
+++ Assets/01.Scripts/UIScripts/BottomMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomMenuScript : MonoBehaviour
+{
+
+    //가챠화면으로 전환
+    public void OnGachaButtonClick()
+    {
+
+    }
+
+    //지도 화면으로 전환
+    public void OnMapButtonClick()
+    {
+
+    }
+
+    //카페 화면으로 전환
+    public void OnCafeButtonClick()
+    {
+
+    }    
+}
---
+++ Assets/01.Scripts/UIScripts/TopNotchScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TopNotchScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _currentTimeText;
+    [SerializeField] private TextMeshProUGUI _currentDateText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+        UpdateDate();
+    }
+
+    void UpdateTime()
+    {
+        _currentTimeText.text = $"{System.DateTime.UtcNow.ToString("HH:mm")}";
+    }
+
+    void UpdateDate()
+    {
+        _currentDateText.text = $"{System.DateTime.UtcNow.ToString("dd MMMM, yyyy")}";
+    }
+}
